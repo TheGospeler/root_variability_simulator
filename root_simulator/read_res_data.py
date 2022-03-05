e6b640be08c7cb7ccb85c613aa5bd6b2b487be5e
John Salako <salakojo@msu.edu>
2022-03-05 17:42:14 +0000

    Updated my read_res_data functionality to help catch errors

--- root_simulator/read_res_data.py
+++ root_simulator/read_res_data.py
@@ -1,6 +1,6 @@
 """importing dependency modules."""
 import numpy as np
-import csv
+import csv, os
 
 def res_data(csv_file, header=0):
 	"""Read in the csv file location.
@@ -17,12 +17,22 @@ def res_data(csv_file, header=0):
 
 	For example, csv_file = '../res_data.csv' or 'res_data.csv' if file is in correct directory.
 	"""
-	res_data = csv.reader(open(csvfile, 'r'), delimiter=',', quotechar='"')
+	if not os.path.isfile(csv_file):
+		raise ValueError("{} file not found".format(csv_file))
+
+	res_data = csv.reader(open(csv_file, 'r'), delimiter=',', quotechar='"')
 	rows = [row for row in res_data]
 
 	rhomap = np.ones([len(rows)-header, 2])
-	for line in rows:
+
+	for line in range(len(rows)):
+		if rows[line][0] not in (int, float):
+			raise TypeError("{} is not a number".format(rows[line][0]))
+
 		rhomap[line-header, 0] = rows[line][0]
 		rhomap[line-header, 1] = rows[line][1]
+
+	if rhomap[:,0][0] == 0 or rhomap[:,1][0] == 0:
+		raise ValueError("{} should have two rows".format(csv_file))
 
 	return rhomap
